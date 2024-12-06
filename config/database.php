83270562a37ac919daf452d7bbd73e0b9ece1ff8
xpop134 <127761568+xpop134@users.noreply.github.com>
2024-12-06 09:25:34 +0800

    Configured database

--- config/database.php
+++ config/database.php
@@ -135,11 +135,12 @@ return [
         ],
 
         'cache' => [
+            'scheme' => 'tls',
             'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
+            'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
+            'password' => env('AZURE_REDIS_PASSWORD', null),
+            'port' => env('AZURE_REDIS_PORT', '6379'),
+            'database' => env('AZURE_REDIS_DATABASE', '1'),
         ],
 
     ],
